e2c71415481c63df839eca3db7888bb14d77819f
TossSky <vyacheslav@totsky.ee>
2025-09-30 23:37:06 +0300

    asdwe

--- payments.py
+++ payments.py
@@ -300,6 +300,9 @@ async def create_stars_invoice(
         provider_token="",  # Empty for Stars
         currency="XTR",  # Telegram Stars currency
         prices=[LabeledPrice(label=plan["description"], amount=plan["price_stars"])],
+        photo_url="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpromokodoff.ru%2Fpromokody-telegram-stars%2F&psig=AOvVaw1TcMs8BlTznDIL3PrYLGZQ&ust=1759350761741000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCrkbqqgZADFQAAAAAdAAAAABAE",
+        photo_width=400,
+        photo_height=250,
         is_flexible=False,
         start_parameter=f"subscription-{plan_type}",
     )
@@ -480,11 +483,6 @@ async def handle_subscribe_callback(update: Update, context: ContextTypes.DEFAUL
         plan = SubscriptionManager.SUBSCRIPTION_PLANS[plan_type]
         user_id = update.effective_user.id
         
-        # Удаляем сообщение с кнопками
-        try:
-            await query.message.delete()
-        except:
-            pass
         
         # Отправляем инвойс для оплаты звёздочками
         await context.bot.send_invoice(
@@ -495,6 +493,9 @@ async def handle_subscribe_callback(update: Update, context: ContextTypes.DEFAUL
             provider_token="",  # Пустой для Stars
             currency="XTR",  # Telegram Stars
             prices=[LabeledPrice(label=plan["description"], amount=plan["price_stars"])],
+            photo_url="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpromokodoff.ru%2Fpromokody-telegram-stars%2F&psig=AOvVaw1TcMs8BlTznDIL3PrYLGZQ&ust=1759350761741000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCrkbqqgZADFQAAAAAdAAAAABAE",
+            photo_width=400,
+            photo_height=250,
             is_flexible=False,
             start_parameter=f"subscription-{plan_type}",
         )
@@ -529,6 +530,9 @@ async def handle_subscribe_callback(update: Update, context: ContextTypes.DEFAUL
             provider_token="",  # Пустой для Stars
             currency="XTR",  # Telegram Stars
             prices=[LabeledPrice(label=plan["description"], amount=plan["price_stars"])],
+            photo_url="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpromokodoff.ru%2Fpromokody-telegram-stars%2F&psig=AOvVaw1TcMs8BlTznDIL3PrYLGZQ&ust=1759350761741000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCrkbqqgZADFQAAAAAdAAAAABAE",
+            photo_width=400,
+            photo_height=250,
             is_flexible=False,
             start_parameter=f"subscription-{plan_type}",
         )
